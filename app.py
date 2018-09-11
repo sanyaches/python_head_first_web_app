0d924fd6302717e15235e2c06871c11135d14467
sanyaches <sanya_ches@mail.ru>
2018-09-11 09:55:00 +0500

    for salim

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ from search import search4letters as s4l
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello Vikaaaaa! <3 <3 <3'
-
-
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(s4l(phrase, letters))
+    results = str(s4l(phrase,letters))
+    title = 'Here are your results:'
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
