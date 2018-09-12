cdb7c373aaab43c4c094fe08d1c747d58f75add1
sanyaches <sanya_ches@mail.ru>
2018-09-12 15:02:53 +0500

    Added web app's log, move to localhost/viewlog

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from search import search4letters as s4l
 
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log','a') as log:
+        print(req.form, req.remote_addr,
+              req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -10,6 +16,7 @@ def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(s4l(phrase,letters))
+    log_request(request, results)
     title = 'Here are your results:'
     return render_template('results.html',
                            the_title=title,
@@ -23,6 +30,21 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == "__main__":
