3ae6cc1a1ec8da014175e50e2ba3fc0086f31506
sanyaches <sanya_ches@mail.ru>
2018-09-26 16:01:08 +0500

    Added Exception Handling + MultiThreading

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, session, copy_current_request_context
 from search import search4letters as s4l
-from DBcm import UseDataBase
-
+from DBcm import UseDataBase, ConnectionError, CredentialError, SQLError
+from checker import check_logged_in
+from time import sleep
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -11,9 +13,21 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
                 'database': 'vsearchlogDB',}
 
 
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+
 def log_request(req: 'flask_request', res: str) -> None:
     """Add web-request to log's DB"""
-
+    sleep(5)
     with UseDataBase(app.config['dbconfig']) as cursor:
         _SQL = """insert into log
                   (phrase, letters, ip, browser_string, results)
@@ -28,10 +42,30 @@ def log_request(req: 'flask_request', res: str) -> None:
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Add web-request to log's DB"""
+        sleep(20)
+        with UseDataBase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                      (phrase, letters, ip, browser_string, results)
+                      values 
+                      (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                  req.form['letters'],
+                                  req.remote_addr,
+                                  req.user_agent.browser,
+                                  res,))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(s4l(phrase,letters))
-    log_request(request, results)
+    try:
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
+    except Exception as err:
+        print('Logging failed with error:', str(err))
     title = 'Here are your results:'
     return render_template('results.html',
                            the_title=title,
@@ -48,18 +82,32 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
     """Display the contents of the log DB as a HTML table"""
-    with UseDataBase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results
-                  from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                           the_title='View Log',
-                           row_titles=titles,
-                           the_data=contents,)
+    try:
+        with UseDataBase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results
+                    from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                               the_title='View Log',
+                               row_titles=titles,
+                               the_data=contents, )
+    except ConnectionError as err:
+        print('Trouble with SQL-server', str(err))
+    except CredentialError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error: ', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
+
+
+app.secret_key = 'YouWillNeverGuessMySecretKey'
 
 
 if __name__ == "__main__":
